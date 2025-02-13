1b84dd862dca64d55774c60e69ac74293e9b8111
Ben Allison <benallison@Bens-MacBook-Pro.local>
2025-02-13 18:01:38 -0500

    Correcting Code and cleaing up to fit questions better

--- Lab4.cpp
+++ Lab4.cpp
@@ -10,17 +10,65 @@ using namespace std;
 int main()
 {
 
-	string creditcardnum;
+	string cardnum; //create string for card number and takes input for it
+	do //Runs 
+	{
+		cout << "Please enter the 8-digit credit card number (enter -1 to quit): " << endl;
+		cin >> cardnum;
 
+		if (cardnum == "-1")  // Checks if user wants to quit
+	{
+		cout << "Thank you for using \"Credit Card Number Validation\" !" << endl; 
+		return 0;
+	}
+	} while (cardnum.length() != 8); // Makes sure 8 digits are inputed or it repeats
+	
+
+	int odd = 0; 
+	for (int i = 1; i < 8; i+=2)
+	{
+		odd += cardnum[i] - '0'; //Calculates sum of odd positions
+	}
+	
+	int even = 0;
+	for (int i = 0; i < 8; i += 2) //Calculates sum of even positions 
+	{
+		even += 2*(int(cardnum[i]) - '0') / 10; // Adds ones place digits
+		even += 2*(int(cardnum[i]) - '0') % 10; // Adds tens place digits
+	}
+
+	int digit = odd + even; //Adds totals 
+
+	if (digit % 10 == 0) //Checks if totals end in 0, by dividing by 10 with a remainder
+	{
+		cout << "Number is valid" << endl;
+	}
+	else 
+	{
+		cout << "Number is invalid." << endl;
+		int correction = (correction + ( 10 - digit % 10)) % 10;
+		cout << "Check digit should have been " << correction << endl;
+	}
+	
+
+	return 0;
+}
+
+
+
+ /* Orginal Code I am swapping out, after I realized a few mistakes from the question to the code
+
+string creditcardnum; //create main string
 	cout << "Please enter the 8-digit credit card number: ";
 	cin >> creditcardnum;
 
-	int step1 = 0;
+	int step1 = 0; //Creates integer 
 
 	for (int i = 1; i < 8; i+=2)
 	{
-		step1 += int(creditcardnum[i]) - 48;
+		step1 += int(creditcardnum[i]) - 48; //Converts to integer becasue ASCII of '0' is 48
 	}
+
 	int step2 = 0;
 	for (int i = 0; i < 8; i += 2)
 	{
@@ -44,4 +92,5 @@ int main()
 
 	system("pause");
 	return 0;
-}
+
+*/ 
