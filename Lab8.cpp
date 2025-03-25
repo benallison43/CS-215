7c533ca9e4d5417483df0cdb343fa1322c2e03ce
Ben Allison <benallison@Bens-MacBook-Pro.local>
2025-03-25 15:19:33 -0400

    added functions

--- Lab8.cpp
+++ Lab8.cpp
@@ -5,3 +5,38 @@
  *Author: Ben Allison
  */
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+void dropTWO(double scores[], int& size) //Removes lowest and highest score
+{
+    sort(scores,scores + size); //Sorts scores in order
+    int remove_end = size;
+
+    for (int i = 0; i < size - 1; ++i) //Remove starting value
+    {
+        scores[i] = scores[i+1];
+    }
+    size--; //adjusts size after removing first value
+
+    size--; //removes largest value
+}
+
+double final_score(double scores[], int size)
+{
+
+
+}
+
+int main ()
+{
+
+
+
+
+
+return 0;
+}
