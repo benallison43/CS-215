f66ba866bb7a62157d36f48bf1dae31c3e1ad313
Ben Allison <benallison@Bens-MacBook-Pro.local>
2025-03-25 16:42:48 -0400

    finsihed code and redid input that created errors in output

--- Lab8.cpp
+++ Lab8.cpp
@@ -7,13 +7,26 @@
 
 #include <iostream>
 #include <string>
-
+#include <iomanip>
+#include <sstream> 
 using namespace std;
 
 
 void dropTWO(double scores[], int& size) //Removes lowest and highest score
 {
-    sort(scores,scores + size); //Sorts scores in order
+    for (int i = 0; i < size - 1; ++i)
+    {
+        for (int j = 0; j < size - i - 1; ++j)
+         {
+            if (scores[j] > scores[j + 1]) 
+            {
+                // Swap the elements
+                double temp = scores[j];
+                scores[j] = scores[j + 1];
+                scores[j + 1] = temp;
+            }
+        }
+    }
     int remove_end = size;
 
     for (int i = 0; i < size - 1; ++i) //Remove starting value
@@ -27,16 +40,74 @@ void dropTWO(double scores[], int& size) //Removes lowest and highest score
 
 double final_score(double scores[], int size)
 {
+    double sum = 0;
+    for (int i = 0; i < size; ++i)
+    { 
+        sum = sum + scores[i];
+    }
 
+    double mean = sum/size;
+
+    return mean; 
 
 }
 
+
+
+
+
 int main ()
 {
+double score[6];
+int size = 0;
+string input;
 
 
+while (size < 6)
+{
+    cout << "Please enter your score for the gymnast:" << endl;
+    getline(cin, input);
+    stringstream ss(input);
+    double ballot;
 
+    if (ss >> ballot) // successfully read a number
+    {
+        if (ballot >= 0.0 && ballot <= 10.0)
+        {
+            score[size] = ballot;
+            size++;
+        }
+        else
+        {
+            cout << "Invalid score! Expecting a score in the range [0.00, 10.00]" << endl;
+        }
+    }
+    else
+    {
+        cout << "Invalid score! Expecting a score in the range [0.00, 10.00]" << endl;
+    }
+}
 
+    
+    cout << "The scores from the judges are:" << endl;
 
-return 0;
+    for (int i = 0; i < 6; ++i)
+    {
+        cout << fixed << setprecision(2) << score[i] << " ";
+    }
+    cout << endl;
+
+    cout << "The scores after dropping the highest and lowest scores:" << endl;
+    int modified_size = 6;
+    dropTWO(score, modified_size);
+    
+    for (int i = 0; i < modified_size; ++i)
+    {
+        cout << fixed << setprecision(2) << score[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Final Execution Score is: " << fixed << setprecision(2) << final_score(score, modified_size) << endl;
+
+    return 0;
 }
